ab0d9ba1c44dace81bf515a71ee6166defb0f347
javierReyVargas <javierre10va@gmail.com>
2023-11-30 10:32:35 -0500

    add item-movies component

---
+++ src/app/components/item-movie/item-movie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'item-movie',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-movie.component.html',
+  styleUrl: './item-movie.component.scss',
+})
+export class ItemMovieComponent {}
