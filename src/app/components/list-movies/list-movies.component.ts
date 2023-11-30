0901d18dda80a545f92f363ed85449f56395f33a
javierReyVargas <javierre10va@gmail.com>
2023-11-30 15:09:07 -0500

    add list item component

--- src/app/components/list-movies/list-movies.component.ts
+++ src/app/components/list-movies/list-movies.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ItemMovieComponent } from '../item-movie/item-movie.component';
+import { MoviesService } from './services/movies.service';
+import { Observable, map, tap } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { IMovie } from '../data/movie.model';
 
 @Component({
   selector: 'app-list-movies',
   standalone: true,
-  imports: [ItemMovieComponent],
+  imports: [ItemMovieComponent, HttpClientModule, CommonModule],
+  providers: [MoviesService],
   templateUrl: './list-movies.component.html',
   styleUrl: './list-movies.component.scss',
 })
-export class ListMoviesComponent {}
+export class ListMoviesComponent {
+  moviesService$: Observable<IMovie[]> = inject(MoviesService).getListMovies();
+}
