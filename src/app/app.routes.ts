89b3b4f0b31add85803a693bd8852ab4e71143c4
javierReyVargas <javierre10va@gmail.com>
2023-11-30 10:08:04 -0500

    add route from app to main component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./components/main/main.routes').then((m) => m.MAIN_ROUTES),
+  },
+];
