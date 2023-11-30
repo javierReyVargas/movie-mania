1f550948b37275c5592aef6cc0dcc7825ede0c23
javierReyVargas <javierre10va@gmail.com>
2023-11-30 15:07:43 -0500

    add item movie components

--- src/app/components/item-movie/item-movie.component.ts
+++ src/app/components/item-movie/item-movie.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'item-movie',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, NgIf],
   templateUrl: './item-movie.component.html',
   styleUrl: './item-movie.component.scss',
 })
-export class ItemMovieComponent {}
+export class ItemMovieComponent {
+  @Input() onWatchList: boolean = false;
+  @Input() urlImage!: string;
+  @Input() title: string = '';
+  @Input() description: string = '';
+}
