31c77a436dde7301b5f44a88366b71f5ad003177
javierReyVargas <javierre10va@gmail.com>
2023-11-30 10:33:18 -0500

    change main component structure

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
--- src/app/components/main/main.routes.ts
+++ src/app/components/main/main.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main.component';
+import { ListMoviesComponent } from '../list-movies/list-movies.component';
 
 export const MAIN_ROUTES: Routes = [
   {
     path: '',
     component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: ListMoviesComponent,
+      },
+    ],
   },
 ];
