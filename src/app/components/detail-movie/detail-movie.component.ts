5619cd2ec643e012c16f1e13b0ebbd38bdc76187
javierReyVargas <javierre10va@gmail.com>
2023-12-02 16:47:08 -0500

    add detail component

---
+++ src/app/components/detail-movie/detail-movie.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { IMovie } from '../data/movie.model';
+import { MoviesService } from '../list-movies/services/movies.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-detail-movie',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    RouterLink,
+  ],
+  providers: [MoviesService],
+  templateUrl: './detail-movie.component.html',
+  styleUrl: './detail-movie.component.scss',
+})
+export class DetailMovieComponent implements OnInit {
+  @Input() id!: number;
+  movieService = inject(MoviesService);
+  movieDetail$!: Observable<IMovie>;
+  sanitizer: DomSanitizer = inject(DomSanitizer);
+
+  ngOnInit(): void {
+    this.movieDetail$ = this.movieService.getMovieById(+this.id).pipe(
+      map((movie: IMovie) => {
+        console.log(movie.trailer as string);
+        return {
+          ...movie,
+          trailer: this.sanitizer.bypassSecurityTrustResourceUrl(
+            movie.trailer
+          ) as string,
+        };
+      })
+    );
+  }
+  onChangeWatchList(event: any, movie: IMovie) {
+    console.log(event.checked);
+    event.checked
+      ? this.movieService.addMovieToWatchList(movie)
+      : this.movieService.removeMovieToWatchList(movie);
+  }
+}
