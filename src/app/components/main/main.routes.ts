3406970053de242bf3f415c7c6baf668b195846f
javierReyVargas <javierre10va@gmail.com>
2023-12-04 17:37:20 -0500

    add new path

--- src/app/components/main/main.routes.ts
+++ src/app/components/main/main.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main.component';
 import { ListMoviesComponent } from '../list-movies/list-movies.component';
 import { DetailMovieComponent } from '../detail-movie/detail-movie.component';
+import { WatchListComponent } from '../watch-list/watch-list.component';
 
 export const MAIN_ROUTES: Routes = [
   {
@@ -15,6 +16,10 @@ export const MAIN_ROUTES: Routes = [
       {
         path: 'detail',
         component: DetailMovieComponent,
+      },
+      {
+        path: 'watchlist',
+        component: WatchListComponent,
       },
     ],
   },
