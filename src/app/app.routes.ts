852aa5398464f577aea7b2626dcd4d080db97adf
javierReyVargas <javierre10va@gmail.com>
2023-12-04 20:19:44 -0500

    change to run deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    title: 'Main',
     loadChildren: () =>
       import('./components/main/main.routes').then((m) => m.MAIN_ROUTES),
   },
