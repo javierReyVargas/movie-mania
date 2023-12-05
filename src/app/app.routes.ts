acb6c7f1aa479c16a8db067b9a518753a881cd97
javierReyVargas <javierre10va@gmail.com>
2023-12-04 19:28:41 -0500

    cleanup

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./components/main/main.routes').then((m) => m.MAIN_ROUTES),
-  },
-];
