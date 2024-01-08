2182908e4f168c7b84982fa8e1789f1d0c904434
javierReyVargas <javierre10va@gmail.com>
2024-01-08 09:55:23 -0500

    *provide httpClient to the app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideAnimations(),
+    provideHttpClient(),
   ],
 };
